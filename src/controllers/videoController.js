31c5c105b1aa52ee7ad02b3e3134eda8a33146c9
verobeach7 <developer.yun7@gmail.com>
2022-09-04 18:19:23 +0900

    Pug Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,14 @@
 const fakeUser = {
   username: "verobeach",
   loggedIn: true,
+  //loggedIn: false,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  //const videos = [];
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8];
+  res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const deleteVideo = (req, res) => {
