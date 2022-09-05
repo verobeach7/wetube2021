8fc20f668ae2c64353da17b8d3f279bd6e9af98d
verobeach7 <developer.yun7@gmail.com>
2022-09-05 21:47:42 +0900

    Edit Video - express.urlencoded, method get/post

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -32,11 +32,18 @@ export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("watch", {
-    pageTitle: `Watching ${video.title}`,
+    pageTitle: `Watching: ${video.title}`,
     video,
   });
 };
-export const edit = (req, res) => res.render("Edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("Edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
