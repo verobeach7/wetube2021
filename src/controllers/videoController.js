d552eb87d7bd5c99ee881bc033338092fa5d7581
verobeach7 <verobeach7@gmail.com>
2022-09-09 17:07:28 +0900

    Return and Render

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,29 +1,9 @@
 import Video from "../models/Video";
 
-/* Callbakc Function
-export const home = (req, res) => {
-  console.log("First");
-  Video.find({}, (error, videos) => {
-    if(error) {
-      return res.render("server-error")
-    }
-    console.log("Second");
-    res.render("home", { pageTitle: "Home", videos: [] });
-    return console.log("Third");
-  });
-  console.log("Fourth");
-};
-*/
-
-/* Promise(async and await) */
 export const home = async (req, res) => {
   try {
-    console.log("First");
     const videos = await Video.find({});
-    console.log("Second");
-    console.log(videos);
     res.render("home", { pageTitle: "Home", videos });
-    return console.log("Third");
   } catch {
     return res.render("server-error");
   }
