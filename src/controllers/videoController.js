c0602883d618545f649f2caacc9a811e8bbab551
verobeach7 <verobeach7@gmail.com>
2022-09-10 15:54:24 +0900

    Mongoose: Exception and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,31 +29,17 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  // Document 생성
-  /* 첫번째 방법: javascript로 만들고 db에 save하는 방식
-  const video = new Video({
-    title, //title: title 왼쪽은 schema의 title, 오른쪽은 req.body의 title
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  await video.save();
-  */
-  /* 두번째 방식: 바로 db에 create하는 방식 */
-  await Video.create({
-    title, //title: title 왼쪽은 schema의 title, 오른쪽은 req.body의 title
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
