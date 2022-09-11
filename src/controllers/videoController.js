6907cf340753c05636a3e035dbd2385d99a86d6d
verobeach7 <developer.yun7@gmail.com>
2022-09-11 16:57:47 +0900

    Pre Middleware

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -21,6 +21,7 @@ export const watch = async (req, res) => {
     });
   }
 };
+
 export const getEdit = async (req, res) => {
   const { id } = req.params;
   // findById를 사용해야만 함, res.render에서 video 오브젝트를 보내줘야 하기 때문임
@@ -30,22 +31,14 @@ export const getEdit = async (req, res) => {
   }
   res.render("Edit", { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  //const video = await Video.findById(id);
   const video = await Video.exists({ _id: id });
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
-  /*
-  video.title = title;
-  video.description = description;
-  video.hashtags = hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-  await video.save();
-  */
   await Video.findByIdAndUpdate(id, {
     title,
     description,
@@ -55,18 +48,18 @@ export const postEdit = async (req, res) => {
   });
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags,
     });
     return res.redirect("/");
   } catch (error) {
