cad8cbe1ac357d5c84f631d0df3975e9d803a856
verobeach7 <developer.yun7@gmail.com>
2022-10-03 16:36:45 +0900

    Better Developer Experience

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  mode: "development",
+  watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
   ],
-  mode: "development",
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "assets"),
+    // clean: webpack이 재시작될 때 정리해주는 기능
+    clean: true,
   },
   module: {
     rules: [
