c811012160b006c17c73011084dc30a1d25cd711
verobeach7 <developer.yun7@gmail.com>
2022-10-09 14:25:06 +0900

    WebAssembly Video Transcode: Thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,5 +1,4 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
-import { async } from "regenerator-runtime";
 
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
@@ -11,22 +10,56 @@ let videoFile;
 const handleDownload = async () => {
   const ffmpeg = createFFmpeg({ log: true });
   await ffmpeg.load();
+
   // FS: File System, (method, filename, whichRawData)
   ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
   // -i: Input, -r: Set frame rate
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss",
+    "00:00:01",
+    "-frames:v",
+    "1",
+    "thumbnail.jpg"
+  );
 
+  // 파일 읽기
   const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbnailFile = ffmpeg.FS("readFile", "thumbnail.jpg");
 
+  // buffer 안에 실제 raw data(binary data)를 가지고 파일 형식의 Blob 생성
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbnailBlob = new Blob([thumbnailFile.buffer], { type: "image/jpg" });
 
+  // 생성된 파일의 url 생성
   const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbnailUrl = URL.createObjectURL(thumbnailBlob);
 
+  // 파일 url을 html Doc에 삽입
   const a = document.createElement("a");
   a.href = mp4Url;
   a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
+
+  const thumbnailA = document.createElement("a");
+  thumbnailA.href = thumbnailUrl;
+  thumbnailA.download = "MyThumbnail.jpg";
+  document.body.appendChild(thumbnailA);
+  thumbnailA.click();
+
+  // 브라우저 속도 저하를 막기 위해서 파일 연결을 끊어주기
+  ffmpeg.FS("unlink", "recording.webm");
+  ffmpeg.FS("unlink", "output.mp4");
+  ffmpeg.FS("unlink", "thumbnail.jpg");
+
+  // url 링크도 끊어주기
+  URL.revokeObjectURL(mp4Url);
+  URL.revokeObjectURL(thumbnailUrl);
+  URL.revokeObjectURL(videoFile);
 };
 
 const handleStop = () => {
