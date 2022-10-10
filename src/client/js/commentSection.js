94cc93238dee5fc80f129b1098a2c37e318af016
verobeach7 <developer.yun7@gmail.com>
2022-10-10 13:01:49 +0900

    Comment: Rendering Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,7 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
@@ -9,7 +9,7 @@ const handleSubmit = (event) => {
   if (text.trim() === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     // express에게 json으로 보낼 것이라는 것을 반드시 알려야 함. 헤더에 포함시키지 않으면 텍스트를 보내는 것으로 인식함.
     headers: {
@@ -18,7 +18,10 @@ const handleSubmit = (event) => {
     // 프론트엔드에서 JSON.stringify를 사용하여 string으로 구성해줌, 백엔드에서 string을 받아 JS object로 바꿔서 사용함
     body: JSON.stringify({ text }),
   });
+  console.log(response);
   textarea.value = "";
+  // 페이지 새로고침으로 작동함
+  window.location.reload();
 };
 
 if (form) {
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,7 +15,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   } else {
@@ -164,11 +165,21 @@ export const createComment = async (req, res) => {
   if (!video) {
     return res.sendStatus(404);
   }
+
+  const userDB = await User.findById(user._id);
+  if (!user) {
+    return res.sendStatus(404);
+  }
+  console.log(user);
   const comment = await Comment.create({
     text,
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
+  userDB.comments.push(comment._id);
+  userDB.save();
 
   return res.sendStatus(201);
 };
